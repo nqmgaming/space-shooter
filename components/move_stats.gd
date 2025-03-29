0e8d173d01b73f60373438de647c8dc15adeb70d
Nguyễn Quang Minh (NQM) <nguyenquangminh391@gmail.com>
2025-03-29 11:25:48 +0700

    laser shoot

---
+++ components/move_stats.gd
@@ -0,0 +1,4 @@
+class_name MoveStats
+extends Resource
+
+@export var spped: int = 100
---
+++ projectiles/laser.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
+@onready var move_component: MoveComponent = $MoveComponent as MoveComponent
+@onready var scale_component: ScaleComponent = $ScaleComponent as ScaleComponent
+@onready var flash_component: FlashComponent = $FlashComponent as FlashComponent
+
+func _ready() -> void:
+	scale_component.tween_scale()
+	flash_component.flash()
+	visible_on_screen_notifier_2d.screen_exited.connect(on_screen_exited)
+	move_component.velocity = Vector2(0, -200) # Move upward with speed 200
+	
+func on_screen_exited() -> void:
+	print("Laser exited screen - destroying")
+	queue_free()
+	
+func _process(_delta: float) -> void:
+	# As a fallback, manually check if we're outside the screen boundaries
+	var screen_size = get_viewport_rect().size
+	if global_position.y < -50 or global_position.y > screen_size.y + 50 or \
+	   global_position.x < -50 or global_position.x > screen_size.x + 50:
+		print("Laser manually detected outside screen - destroying")
+		queue_free()
