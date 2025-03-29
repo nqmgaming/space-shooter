0526f4697b41499899b2246bc63d29d0431e37a8
Nguyễn Quang Minh (NQM) <nguyenquangminh391@gmail.com>
2025-03-29 11:49:52 +0700

    feat: animating the ship

--- player_ship/ship.gd
+++ player_ship/ship.gd
@@ -6,6 +6,10 @@ extends Node2D
 @onready var spawner_component: SpawnerComponent = $SpawnerComponent as SpawnerComponent
 @onready var fire_rate_timer: Timer = $FireRateTimer
 @onready var scale_component: ScaleComponent = $ScaleComponent
+@onready var animated_sprite_2d: AnimatedSprite2D = $Anchor/AnimatedSprite2D
+@onready var move_component: MoveComponent = $MoveComponent
+@onready var flame_animated_sprite: AnimatedSprite2D = %FlameAnimatedSprite
+
 
 
 func _ready() -> void:
@@ -16,3 +20,18 @@ func fire_lasers() -> void:
 	spawner_component.spawn(left_muzzle.global_position)
 	spawner_component.spawn(right_muzzle.global_position)
 	scale_component.tween_scale()
+	
+func _process(delta: float) -> void:
+	animate_the_ship()
+	
+func animate_the_ship() -> void:
+	if move_component.velocity.x < 0:
+		animated_sprite_2d.play("bank_left")
+		flame_animated_sprite.play("bank_left")
+	elif move_component.velocity.x > 0:
+		animated_sprite_2d.play("bank_right")
+		flame_animated_sprite.play("bank_right")
+	else:
+		animated_sprite_2d.play("center")
+		flame_animated_sprite.play("center")
+	pass
