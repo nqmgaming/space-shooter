83533add4733edfc2b00b6f2c83f5d0a0d98c8fa
Nguyễn Quang Minh (NQM) <nguyenquangminh391@gmail.com>
2025-03-29 16:49:16 +0700

    feat: hurt component

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -6,7 +6,14 @@ extends Node2D
 @onready var scale_component: ScaleComponent = $ScaleComponent as ScaleComponent
 @onready var flash_component: FlashComponent = $FlashComponent as FlashComponent
 @onready var shake_component: ShakeComponent = $ShakeComponent as ShakeComponent
-
+@onready var hitbox_component: HitboxComponent = $HitboxComponent
+@onready var hurtbox_component: HurtboxComponent = $HurtboxComponent
 
 func _ready() -> void:
 	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
+	hurtbox_component.hurt.connect(func(hitbox: HitboxComponent):
+		scale_component.tween_scale()
+		flash_component.flash()
+		shake_component.tween_shake()
+		)
+	stats_component.no_health.connect(queue_free)
--- projectiles/laser.gd
+++ projectiles/laser.gd
@@ -4,11 +4,13 @@ extends Node2D
 @onready var move_component: MoveComponent = $MoveComponent as MoveComponent
 @onready var scale_component: ScaleComponent = $ScaleComponent as ScaleComponent
 @onready var flash_component: FlashComponent = $FlashComponent as FlashComponent
+@onready var hitbox_component: HitboxComponent = $HitboxComponent
 
 func _ready() -> void:
 	scale_component.tween_scale()
 	flash_component.flash()
 	visible_on_screen_notifier_2d.screen_exited.connect(on_screen_exited)
+	hitbox_component.hit_hurtbox.connect(queue_free.unbind(1))
 	
 func on_screen_exited() -> void:
 	print("Laser exited screen - destroying")
